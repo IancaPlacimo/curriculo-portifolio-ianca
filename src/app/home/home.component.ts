1bcb833e695f03811af7167d31ef1c54abc2b8b1
IancaPlacimo <iancaplacimo@gmail.com>
2024-07-24 19:32:18 -0300

    ajustando font e buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,48 @@
-import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { ApiServiceService } from '../api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-
 export class HomeComponent {
-  constructor(private router:Router, private api : ApiServiceService ){
+  texts: string[] = [
+    'Front-end',
+    'Angular',
+    'HTML',
+    'CSS',
+    'Typescript',
+    'JavaScript',
+    'Web Responsive',
+    'UI/UX',
+    'Bootstrap',
+    'SASS',
+    'Node.js',
+  ];
+  currentText: string = this.texts[0];
+  count: number = 0;
+
+  constructor(private router: Router, private api: ApiServiceService) {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.changeText();
+    }, 1000);
   }
 
-  contatos(){
+  changeText(): void {
+    this.count = (this.count + 1) % this.texts.length;
+    this.currentText = this.texts[this.count];
+  }
+
+  contatos() {
     this.router.navigate(['/contatos']);
   }
 
-  download(){
-    this.api.downloadPDF().subscribe(res => {
+  download() {
+    this.api.downloadPDF().subscribe((res) => {
       let url = window.URL.createObjectURL(res);
       let a = document.createElement('a');
       a.href = url;
@@ -27,6 +50,6 @@ export class HomeComponent {
       a.click();
       window.URL.revokeObjectURL(url);
       a.remove();
-    })
+    });
   }
 }
